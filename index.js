630100a6b08de62ed514c885bc67e54c507e377e
spenser6131 <brinkman.spenser@gmail.com>
2021-01-11 08:59:27 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser = "Spenser";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
