d2f50bd451e3eb4040c306df68cf9658aad6263d
spenser6131 <brinkman.spenser@gmail.com>
2021-01-21 10:53:45 -0800

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let currentUser = "Spenser";
 let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
 let shortGreeting = `Welcome, ${currentUser[0]}!`;
